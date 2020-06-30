8cb607e80a8f7cd39a4c2cca54da52531ba605fc
Jexxie <1989wjx@gmail.com>
2020-06-30 15:28:51 +0800

    fix: configs

--- packages/vue/index.js
+++ packages/vue/index.js
@@ -1,6 +1,9 @@
 module.exports = {
-	parser: '@typescript-eslint/parser',
-	extends: ['@antfu/eslint-config'],
-	plugins: [],
+	parser: 'vue-eslint-parser',
+	parserOptions: {
+		parser: '@typescript-eslint/parser',
+	},
+	extends: ['@wjx0820/my-eslint-config', 'plugin:vue/recommended'],
+	plugins: ['vue'],
 	rules: {},
 }
